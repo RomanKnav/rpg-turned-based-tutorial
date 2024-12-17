6aded9784a7b650d25347e85c83e4e184d4643af
Roman Navarro <ronnoverro@gmail.com>
2024-12-16 23:57:32 -0800

    init

---
+++ Assets/TurnBattleSystem/Scripts/BattleHandler.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BattleHandler : MonoBehaviour {
+
+    private static BattleHandler instance;
+
+    public static BattleHandler GetInstance() {
+        return instance;
+    }
+
+    // wtf are CharacterBattle objs? the characters themselves
+    [SerializeField] private Transform pfCharacterBattle;     // contains instance of "pfCharacterBattle" prefab
+    public Texture2D playerSpritesheet;
+    public Texture2D enemySpritesheet;
+
+    private CharacterBattle playerCharacterBattle;
+    private CharacterBattle enemyCharacterBattle;
+    private CharacterBattle activeCharacterBattle;
+    private State state;
+
+    private enum State {
+        WaitingForPlayer,
+        Busy,
+    }
+
+    private void Awake() {
+        instance = this;
+    }
+
+    private void Start() {
+        playerCharacterBattle = SpawnCharacter(true);
+        enemyCharacterBattle = SpawnCharacter(false);
+
+        SetActiveCharacterBattle(playerCharacterBattle);
+        state = State.WaitingForPlayer;
+    }
+
+    private void Update() {
+        if (state == State.WaitingForPlayer) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                // if space is pressed down, player is "busy" and initiates attack:
+                state = State.Busy;
+                playerCharacterBattle.Attack(enemyCharacterBattle, () => {
+                    ChooseNextActiveCharacter();
+                });
+            }
+        }
+    }
+
+    private CharacterBattle SpawnCharacter(bool isPlayerTeam) {
+        Vector3 position;
+        if (isPlayerTeam) {
+            position = new Vector3(-50, 0);     // positioned in relation to what?
+        } else {
+            position = new Vector3(+50, 0);
+        }
+        // how to position objects on-screen via code:
+        Transform characterTransform = Instantiate(pfCharacterBattle, position, Quaternion.identity);     
+        CharacterBattle characterBattle = characterTransform.GetComponent<CharacterBattle>();
+        characterBattle.Setup(isPlayerTeam);
+
+        return characterBattle;
+    }
+
+    private void SetActiveCharacterBattle(CharacterBattle characterBattle) {
+        if (activeCharacterBattle != null) {
+            activeCharacterBattle.HideSelectionCircle();
+        }
+
+        activeCharacterBattle = characterBattle;
+        activeCharacterBattle.ShowSelectionCircle();
+    }
+
+    private void ChooseNextActiveCharacter() {
+        if (TestBattleOver()) {
+            return;
+        }
+
+        if (activeCharacterBattle == playerCharacterBattle) {
+            SetActiveCharacterBattle(enemyCharacterBattle);
+            state = State.Busy;
+            
+            enemyCharacterBattle.Attack(playerCharacterBattle, () => {
+                ChooseNextActiveCharacter();
+            });
+        } else {
+            SetActiveCharacterBattle(playerCharacterBattle);
+            state = State.WaitingForPlayer;
+        }
+    }
+
+    private bool TestBattleOver() {
+        if (playerCharacterBattle.IsDead()) {
+            // Player dead, enemy wins
+            //CodeMonkey.CMDebug.TextPopupMouse("Enemy Wins!");
+            BattleOverWindow.Show_Static("Enemy Wins!");
+            return true;
+        }
+        if (enemyCharacterBattle.IsDead()) {
+            // Enemy dead, player wins
+            //CodeMonkey.CMDebug.TextPopupMouse("Player Wins!");
+            BattleOverWindow.Show_Static("Player Wins!");
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/TurnBattleSystem/Scripts/BattleOverWindow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// script is put on: UI/Canvas/BattleOverWindow
+public class BattleOverWindow : MonoBehaviour {
+
+    private static BattleOverWindow instance;
+
+    private void Awake() {
+        instance = this;
+        // initialled deactivated:
+        Hide();
+    }
+
+    private void Hide() {
+        gameObject.SetActive(false);
+    }
+
+    private void Show(string winnerString) {
+        gameObject.SetActive(true);
+
+        transform.Find("winnerText").GetComponent<Text>().text = winnerString;
+    }
+
+    public static void Show_Static(string winnerString) {
+        instance.Show(winnerString);
+    }
+}
