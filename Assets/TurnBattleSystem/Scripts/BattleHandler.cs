9105fb7174c0355a9caf75f6ad25f661c58ddda7
roman <ronnoverro@gmail.com>
2024-12-17 20:41:52 -0800

    macbook init

---
+++ Assets/TurnBattleSystem/Scripts/BattleHandler.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// script put on empty BattleHandler object.
+// this script is PASSED the prefab instances of the player and enemy. 
+
+public class BattleHandler : MonoBehaviour {
+
+    // what are static instances? MAKES IT A FUCKING SINGLETON!!!!!
+    // a single, globally accessible object created from a class with static members (variables and methods)
+    private static BattleHandler instance;
+
+    public static BattleHandler GetInstance() {
+        return instance;
+    }
+
+    // wtf are CharacterBattle objs? the characters themselves
+    [SerializeField] private Transform pfCharacterBattle;     // HERE'S WHERE WE PUT THE "pfCharacterBattle" PREFAB
+
+    // shit not even used in here. Used in CharacterBattle:
+    public Texture2D playerSpritesheet;
+    public Texture2D enemySpritesheet;
+
+    // how are these assigned? via SpawnCharacter():
+    private CharacterBattle playerCharacterBattle;             // these are PREFAB instances
+    private CharacterBattle playerCharacterBattle2;
+
+
+    private CharacterBattle enemyCharacterBattle;
+    private CharacterBattle activeCharacterBattle;          // what determines current character
+    private State state;
+
+    private enum State {
+        WaitingForPlayer,
+        Busy,
+    }
+
+    // MY OWN CRAP:
+    // TODO: make a list of all the friendly character objects:
+    private static List<CharacterBattle> friendlies = new List<CharacterBattle>();
+
+    private void Awake() {
+        instance = this;
+    }
+
+    private void Start() {
+        // this causes the characters to not be drawn UNTIL game is initiated (fucking crazy):
+        playerCharacterBattle = SpawnCharacter(true);
+        playerCharacterBattle2 = SpawnCharacter(true);
+
+        friendlies.Add(playerCharacterBattle);
+        friendlies.Add(playerCharacterBattle2);
+
+        enemyCharacterBattle = SpawnCharacter(false);       // why this set to false?
+
+        SetActiveCharacterBattle(playerCharacterBattle);
+
+        // initial character state:
+        state = State.WaitingForPlayer;     
+    }
+
+    private void Update() {
+        if (state == State.WaitingForPlayer) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+
+                // if space is pressed down, player is "busy" and initiates attack:
+                state = State.Busy;
+
+                // where's this "Attack" function from? in CharacterBattle.cs:
+                playerCharacterBattle.Attack(enemyCharacterBattle, () => {
+                    ChooseNextActiveCharacter();
+                });
+            }
+        }
+    }
+
+    private CharacterBattle SpawnCharacter(bool isPlayerTeam) {
+        Vector3 position;
+        // if (isPlayerTeam) {
+        //     foreach(CharacterBattle character in friendlies)
+        //     {
+        //         // position = new Vector3(-50, 0);
+        //     }
+        // else
+        // {
+        //     position = new Vector3(+50, 0);
+        // }
+
+
+        if (isPlayerTeam) {
+            position = new Vector3(-50, 0);     // positioned in relation to what? (drawn in vertical center)
+        } else {
+            position = new Vector3(+50, 0);
+        }
+
+        // this creates fucking CLONES:
+        Transform characterTransform = Instantiate(pfCharacterBattle, position, Quaternion.identity);    
+
+        // this component is a SCRIPT: 
+        CharacterBattle characterBattle = characterTransform.GetComponent<CharacterBattle>();
+        characterBattle.Setup(isPlayerTeam);
+
+        return characterBattle;
+    }
+
+    private void SetActiveCharacterBattle(CharacterBattle characterBattle) {
+        if (activeCharacterBattle != null) {
+            activeCharacterBattle.HideSelectionCircle();
+        }
+
+        activeCharacterBattle = characterBattle;
+        activeCharacterBattle.ShowSelectionCircle();
+    }
+
+    private void ChooseNextActiveCharacter() {
+        if (TestBattleOver()) {
+            return;
+        }
+
+        if (activeCharacterBattle == playerCharacterBattle) {
+            SetActiveCharacterBattle(enemyCharacterBattle);
+            state = State.Busy;
+            
+            enemyCharacterBattle.Attack(playerCharacterBattle, () => {
+                ChooseNextActiveCharacter();        // AAHHHH RECURSION
+            });
+        }
+        else if (activeCharacterBattle == playerCharacterBattle2) {
+            SetActiveCharacterBattle(enemyCharacterBattle);
+            state = State.Busy;
+
+            enemyCharacterBattle.Attack(playerCharacterBattle2, () => {
+                ChooseNextActiveCharacter();
+            });
+
+        } else {
+            SetActiveCharacterBattle(playerCharacterBattle);
+            state = State.WaitingForPlayer;
+        }
+    }
+
+    private bool TestBattleOver() {
+        if (playerCharacterBattle.IsDead() && playerCharacterBattle2.IsDead()) {
+            // Player dead, enemy wins
+            //CodeMonkey.CMDebug.TextPopupMouse("Enemy Wins!");
+            BattleOverWindow.Show_Static("Enemy Wins!");
+            return true;
+        }
+        if (enemyCharacterBattle.IsDead()) {
+            // Enemy dead, player wins
+            //CodeMonkey.CMDebug.TextPopupMouse("Player Wins!");
+            BattleOverWindow.Show_Static("Player Wins!");
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/TurnBattleSystem/Scripts/CharacterBattle.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;     // wtf is this??? in Assets/_/Stuff/CodeMonkey/Utils
+
+// what this script do? Responsible for all the ANIMATION and texture-setting crap of the characters.
+
+// script placed on: pfCharacterBattle prefab (ON PREFAB ITSELF)
+// TWO scripts placed on the prefab. This one, and Character_Base.cs.
+public class CharacterBattle : MonoBehaviour {
+
+    private Character_Base characterBase;
+    private State state;
+    private Vector3 slideTargetPosition;
+    private Action onSlideComplete;
+    private bool isPlayerTeam;
+    private GameObject selectionCircleGameObject;
+    private HealthSystem healthSystem;
+    private World_Bar healthBar;
+
+    private enum State {
+        Idle,
+        Sliding,
+        Busy,
+    }
+
+    private void Awake() {
+        characterBase = GetComponent<Character_Base>(); // this is a component??? yes, it's the fucking script!!!
+
+        selectionCircleGameObject = transform.Find("SelectionCircle").gameObject;
+        HideSelectionCircle();
+        state = State.Idle;
+    }
+
+    private void Start() {
+        // Vector3's are for 3D games! why being used here?
+        characterBase.PlayAnimMove(new Vector3(1, 0));
+    }
+
+    public void Setup(bool isPlayerTeam) {
+        this.isPlayerTeam = isPlayerTeam;
+        if (isPlayerTeam) {
+            characterBase.SetAnimsSwordTwoHandedBack();
+
+            // set the character's spritesheet!
+            characterBase.GetMaterial().mainTexture = BattleHandler.GetInstance().playerSpritesheet;
+        } else {
+            characterBase.SetAnimsSwordShield();
+            characterBase.GetMaterial().mainTexture = BattleHandler.GetInstance().enemySpritesheet;
+        }
+        healthSystem = new HealthSystem(100);
+        healthBar = new World_Bar(transform, new Vector3(0, 10), new Vector3(12, 1.7f), Color.grey, Color.red, 1f, 100, new World_Bar.Outline { color = Color.black, size = .6f });
+        healthSystem.OnHealthChanged += HealthSystem_OnHealthChanged;
+
+        PlayAnimIdle();
+    }
+
+    private void HealthSystem_OnHealthChanged(object sender, EventArgs e) {
+        healthBar.SetSize(healthSystem.GetHealthPercent());
+    }
+
+    private void PlayAnimIdle() {
+        if (isPlayerTeam) {
+            characterBase.PlayAnimIdle(new Vector3(+1, 0));
+        } else {
+            characterBase.PlayAnimIdle(new Vector3(-1, 0));
+        }
+    }
+
+    private void Update() {
+        switch (state) {
+        case State.Idle:
+            break;
+        case State.Busy:
+            break;
+        case State.Sliding:
+            float slideSpeed = 10f;
+            transform.position += (slideTargetPosition - GetPosition()) * slideSpeed * Time.deltaTime;
+
+            float reachedDistance = 1f;
+            if (Vector3.Distance(GetPosition(), slideTargetPosition) < reachedDistance) {
+                // Arrived at Slide Target Position
+                //transform.position = slideTargetPosition;
+                onSlideComplete();
+            }
+            break;
+        }
+    }
+
+    public Vector3 GetPosition() {
+        return transform.position;
+    }
+
+    public void Damage(CharacterBattle attacker, int damageAmount) {
+        healthSystem.Damage(damageAmount);
+        Vector3 dirFromAttacker = (GetPosition() - attacker.GetPosition()).normalized;
+
+        characterBase.SetColorTint(new Color(1, 0, 0, 1f));
+        Blood_Handler.SpawnBlood(GetPosition(), dirFromAttacker);
+
+        CodeMonkey.Utils.UtilsClass.ShakeCamera(1f, .1f);
+
+        if (healthSystem.IsDead()) {
+            // Died
+            characterBase.PlayAnimLyingUp();
+        }
+    }
+
+    public bool IsDead() {
+        return healthSystem.IsDead();
+    }
+
+    // passed object to attack, and action to take after
+
+    // the script placed on CharacterBattle objs is:
+    public void Attack(CharacterBattle targetCharacterBattle, Action onAttackComplete) {
+
+        // how is this a Vector3?
+        Vector3 slideTargetPosition = targetCharacterBattle.GetPosition() + (GetPosition() - targetCharacterBattle.GetPosition()).normalized * 10f;
+        Vector3 startingPosition = GetPosition();
+
+        // Slide to Target
+        SlideToPosition(slideTargetPosition, () => {
+            // Arrived at Target, attack him
+            state = State.Busy;
+            Vector3 attackDir = (targetCharacterBattle.GetPosition() - GetPosition()).normalized;
+            characterBase.PlayAnimAttack(attackDir, () => {
+                // Target hit
+                int damageAmount = UnityEngine.Random.Range(20, 50);
+                targetCharacterBattle.Damage(this, damageAmount);
+                }, () => {
+                // Attack completed, slide back
+                SlideToPosition(startingPosition, () => {
+                    // Slide back completed, back to idle
+                    state = State.Idle;
+                    characterBase.PlayAnimIdle(attackDir);
+                    onAttackComplete();
+                });
+            });
+        });
+    }
+
+    private void SlideToPosition(Vector3 slideTargetPosition, Action onSlideComplete) {
+        this.slideTargetPosition = slideTargetPosition;
+        this.onSlideComplete = onSlideComplete;
+        state = State.Sliding;
+        if (slideTargetPosition.x > 0) {
+            characterBase.PlayAnimSlideRight();
+        } else {
+            characterBase.PlayAnimSlideLeft();
+        }
+    }
+
+    public void HideSelectionCircle() {
+        selectionCircleGameObject.SetActive(false);
+    }
+
+    public void ShowSelectionCircle() { 
+        selectionCircleGameObject.SetActive(true);
+    }
+
+}
